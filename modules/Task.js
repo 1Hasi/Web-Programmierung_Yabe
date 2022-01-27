845214f5ab542436a52b7ae476e5896b81fdce28
HettlerM <73434392+HettlerM@users.noreply.github.com>
2022-01-27 17:55:04 +0100

    produkt task

--- modules/Task.js
+++ modules/Task.js
@@ -16,13 +16,27 @@ const TaskSchema = mongoose.Schema({
   //  default: false,
   //  },
 
-  title: {
+  //Produkte: ProduktID, UserID produkt_ersteller (Ersteller), Bezeichnung produkt_bezeichnung, produkt_mindestwert, Beschreibung produkt_beschreibung, Bild produkt_bild
+   
+  produkt_ersteller: {
     type: String,
-    required: true
+    required: true,
   },
-  description: {
+  produkt_bezeichnung: {
     type: String,
-    required: true
+    required: true,
+  },
+  produkt_mindestwert: {
+    type: Int,
+    required: true,
+  },
+  produkt_beschreibung: {
+    type: String,
+    required: true,
+  },
+  produkt_bild: {
+    type: String,
+    required: true,
   }
 
 });

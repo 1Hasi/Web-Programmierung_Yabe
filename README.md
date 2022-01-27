e4ea12cef34989ea243c50391e1cad4e03e8c0d6
HettlerM <73434392+HettlerM@users.noreply.github.com>
2022-01-27 11:24:04 +0100

    Viel und komisch

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Web-Programmierung_Yabe
 Projekt: Entwicklung eines Webservers
-Hello Änderung der Datei
-test2
-test3
+
+Beschreibungen Ordner:
+public/ :
+db/connect: zum verbinden mit der datenbank
+modules/Task.js: zum definieren und strukturieren der datenbank
+.env: muss angelegt werden und benötigt variable "MONGO_URI" zum Datenbank verbinden
+.gitignore: sachen werden nich auf github geladen
+package: darin sind die benötigten module nach runterladen in konsole ".npm install" machen
+
+Notizen: 
+Syntax get anfrage:
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//   adding to static assets
+//   SSR
+// })
---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+//const tasks = require('./routes/tasks');
+// Hier wird für datenbankverbindung gebraucht
+//const connectDB = require('./db/connect')
+require('dotenv').config();
+
+//app.get('/hello', (req, res) => {
+//  res.send('Task Manager')
+//})
+
+//app.use('/api/v1/tasks', tasks);
+
+// Middleware: Hier wird die Webside (indexdatei) im ordner public ausgegeben
+ app.use(express.static('./public'));
+
+// server wird auf port 5000 gehostet
+app.listen(5000, () => {
+  console.log('server is listening on port 5000....')
+});
+
+// für alle nicht definierten routen wird fehler ausgegeben
+//app.all('*', (req, res) => {
+// res.status(404).send('resource not found')
+//});
+
+//const port = 5000;
+
+//Hier wird mit der db connectet
+/**
+  const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+**/

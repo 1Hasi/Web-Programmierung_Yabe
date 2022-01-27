0455394b0b9475e75aaea64bae6826688f671dd0
1Hasi <hasolol136@gmail.com>
2022-01-27 19:02:17 +0100

    Produkte abfragen

--- routes/tasks.js
+++ routes/tasks.js
@@ -10,13 +10,26 @@ router.get('/meineprodukte', (req, res) => {
     res.send('hier liste der produkte des user');
 });
 
-router.get('/meineprodukte/produktanlegen', (req, res) => {
+/*router.get('/meineprodukte/produktanlegen', (req, res) => {
     res.send('hier kann der user ein produkt anlegen');
 });
+*/
 
+// Alle Produkte abfragen
+router.get('/meineprodukte/produktanlegen', async (req, res) => {
+    try {
+        const task = await Task.find()
+        res.json(task);
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+});
+
+// Produktanlegen 
 router.post('/meineprodukte/produktanlegen', (req, res) => {
     const task = new Task ({
         produkt_ersteller: req.body.produkt_ersteller,
+        produkt_bezeichnung: req.body.produkt_bezeichnung,
         produkt_mindestwert: req.body.produkt_mindestwert,
         produkt_beschreibung: req.body.produkt_beschreibung,
         produkt_bild: req.body.produkt_bild

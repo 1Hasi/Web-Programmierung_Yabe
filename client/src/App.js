963f39ad4beb0cb90c145ced48ea7f211330249a
1Hasi <hasolol136@gmail.com>
2022-01-28 18:15:35 +0100

    alles scheiße

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,7 @@ class App extends React.Component{
 render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header> 
       </header>
       <p>{this.state.apiResponse}</p>
     </div>

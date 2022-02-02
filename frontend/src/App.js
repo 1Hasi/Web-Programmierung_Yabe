f83b5ee52ae656b2f1cf5d2c73690ba8906673f0
1Hasi <hasolol136@gmail.com>
2022-02-02 14:30:19 +0100

    copypasteamazona

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-class App extends React.Component{
+/*class App extends React.Component{
   constructor(props){
     super(props);
     this.state={apiResponse: ""}
@@ -14,18 +17,40 @@ class App extends React.Component{
 
   componentWillMount(){
     this.callAPI();
+  }*/
+
+
+  function App() {
+    return (
+      <BrowserRouter>
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="/">
+                amazona
+              </a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+            </div>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/product/:id" element={<ProductScreen />}></Route>
+              <Route path="/" element={<HomeScreen />} exact></Route>
+            </Routes>
+          </main>
+          <footer className="row center">All right reserved</footer>
+        </div>
+      </BrowserRouter>
+    );
   }
 
-
-render(){
-  return (
-    <div className="App">
+/* <div className="App">
       <header> 
       </header>
       <p>{this.state.apiResponse}</p>
-    </div>
-   );
-  }
-}
+    </div>*/
 
 export default App;
